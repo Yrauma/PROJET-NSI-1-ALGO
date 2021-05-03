ef4f915d35c8102714d87aeae18c86f5cca4fdb7
Vial Amaury <amaury.vial503@gmail.com>
2021-05-03 12:22:41 +0200

    Ajout du début du code pour les premières tâches

--- test1.py
+++ test1.py
@@ -1,11 +1,26 @@
-print("Hello World")
+from tkinter import *
 
 
-print("hello")
-
-print("TEST1")
+cryptage=Tk()
 
 
-print("jjj")
+cryptage.title("MultiCrypt")                                                                                             #Creation du titre
+cryptage.geometry("700x500")                                                                                             #taille de la page
+cryptage.iconbitmap("logo.ico")                                                                                          #Insertion du logo
+cryptage.config(background="orange")                                                                                     #couleur de fond
 
-print("Nouveau test")
+Zone_de_texte=Entry(cryptage,width=30)                                                                                   #Zone de texte
+Zone_de_texte.place(x=250,y=200)
+Zone_de_texte_2=Entry(cryptage,width=30)
+Zone_de_texte_2.place(x=250,y=150)
+
+
+Titre1= Label(cryptage, text="key")
+Titre1.place(x=330,y=120)
+Titre2=Label(cryptage,text='Message')
+Titre2.place(x=320,y=175)
+
+Bouton_Validation=Button(cryptage,text="Valider")
+Bouton_Validation.place(x=320,y=400)
+
+cryptage.mainloop()
